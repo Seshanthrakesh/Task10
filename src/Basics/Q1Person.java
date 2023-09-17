46760c2bd105f2012129ad2a43f4fc30d2eee898
seshanth <seshanthrakesh1901@gmail.com>
2023-09-17 19:48:01 +0530

    Answer 2 are committed

--- src/Basics/Q1Person.java
+++ src/Basics/Q1Person.java
@@ -1,9 +1,9 @@
 package Basics;
 
-public class Person {
+public class Q1Person {
 private String name;
 private int age;
-public Person(String name,int age)
+public Q1Person(String name,int age)
 {
 	this.name=name;
 	this.age=age;
@@ -16,7 +16,7 @@ public int getage() {
 }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Person Person = new Person("RAKESH",25);
+		Q1Person Person = new Q1Person("RAKESH",25);
 		System.out.println("Name:"+Person.getname());
 		System.out.println("Age:"+Person.getage());
 		
---
+++ src/Basics/Q2Employee.java
@@ -0,0 +1,13 @@
+package Basics;
+
+public class Q2Employee {
+private String firstName;
+private String SecondName;
+private int ID;
+private int salary;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
